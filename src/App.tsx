2817273a00038629b410e167fe2c975cc2138e7d
LinaGoebel <galina283@freenet.de>
2024-11-27 12:52:34 +0100

    add lesson 07

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import "./App.css";
 
+
 //lessons
-import Lesson06 from "./lessons/Lesson06/Lesson06";
+// import Lesson06 from "./lessons/Lesson06/Lesson06";
+import Lesson07 from "./lessons/Lesson07/Lesson07";
 
 //homeworks
 
@@ -10,7 +12,8 @@ import Lesson06 from "./lessons/Lesson06/Lesson06";
 function App() {
   return (
     <div className="App">
-      <Lesson06 />
+      {/* <Lesson06 /> */}
+      <Lesson07 />
     </div>
   );
 }
--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -1,25 +1,30 @@
-import Button from "../Button/Button";
 import "./styles.css";
+import Button from "../Button/Button";
 //Шаг 1 - импорт хука useState из react
 import { useState } from "react";
 
 function Counter() {
-  ////Шаг 2 - вызываем хук useState и передаем в него первоначальное состояние(initialState)
+  console.log("Render");
+
+  //Шаг 2 - вызываем хук useState и передаем в него первоначальное состояние(initialState)
   //делаем деструктуризацию массива из двух элементов, который возвращает хук useState
-  const [count, setCount] = useState(0);
-  //   const result = useState(0);
-  //   console.log(result);
-  //   const count = result[0];
-  //   console.log(count);
-  //   const setCount = result[1];
-  //   console.log(setCount);
+  // const [count, setCount] = useState<number|undefined>(0);
+  const [count, setCount] = useState<number>(0);
+
+  // const result = useState(0);
+  // console.log(result);
+  // const count = result[0];
+  // console.log(count);
+  // const setCount = result[1];
+  // console.log(setCount);
+
 
   //Шаг 3 - необходимо прописать функции, которые будут менять состояние. Внутри этих функций нужно использовать setCount
-  const onPlusClick = () => {
+  const onPlusClick = (): void => {
     setCount((prevValue) => prevValue + 1);
   };
 
-  const onMinusClick = () => {
+  const onMinusClick = (): void => {
     setCount((prevValue) => prevValue - 1);
   };
 
@@ -28,7 +33,7 @@ function Counter() {
       <div className="button-wrapper">
         <Button name="-" onClick={onMinusClick} />
       </div>
-      <div className="conter-result">{count}</div>
+      <div className="counter-result">{count}</div>
       <div className="button-wrapper">
         <Button name="+" onClick={onPlusClick} />
       </div>
