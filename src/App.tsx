a8e5485bc40462d5a1c862144d67a802f08f23e2
LinaGoebel <galina283@freenet.de>
2024-12-04 19:59:15 +0100

    add lesson 09

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,12 @@ import GlobalStyles from "./styles/GlobalStyles";
 // import Lesson06 from "./lessons/Lesson06/Lesson06";
 // import Lesson07 from "./lessons/Lesson07/Lesson07";
 // import Lesson08 from "./lessons/Lesson08/Lesson08";
+import Lesson09 from "lessons/Lesson09/Lesson09";
 
 //homeworks
 // import Homework07 from "./homeworks/Homeworks07/Homeworks07";
-import Homework08 from "./homeworks/Homework08/Homework08";
+// import Homework08 from "./homeworks/Homework08/Homework08";
+
 
 //comsultations
 // import Consultation04 from "./consultations/Consultation04/Consultation04";
@@ -21,7 +23,8 @@ function App() {
       {/* <Homework07 /> */}
       {/* <Lesson08 /> */}
       {/* <Consultation04 /> */}
-      <Homework08 />
+      {/* <Homework08 /> */}
+      <Lesson09/>
     </>
   );
 }
---
+++ src/lessons/Lesson09/styles.ts
@@ -0,0 +1,15 @@
+import styled from "@emotion/styled";
+
+export const Lesson09Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+  gap: 30px;
+`;
+
+export const ResultBlock = styled.div`
+  padding: 10px;
+  border: 2px solid blueviolet;
+`;
